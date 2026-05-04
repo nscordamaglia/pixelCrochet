acf89f57a83db5f1418c62a270047338c4c8cab8
Nicolas Scordamaglia <nicolas.scordamaglia@contabilium.com>
2026-05-03 22:40:14 -0300

    add automatic deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/pixelCrochet/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
